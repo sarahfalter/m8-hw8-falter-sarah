0a46fea010a3af5463aa876850e3e46fcc4d297c
sarahfalter <sarah.falter@ufl.edu>
2021-07-01 23:22:01 -0400

    Add js and css styling for dropdown

--- hw8-starter-files/main.js
+++ hw8-starter-files/main.js
@@ -1,1 +1,14 @@
 // It is always helpful to use comments in your code!
+
+
+var nav_links = document.getElementsByClassName('main-menu-labels');
+var dropdowns = document.getElementsByClassName('sub-menu');
+
+function toggleDropdown(){
+    this.children[1].classList.toggle('hidden')
+}
+
+for(i=0; i<nav_links.length; i++){
+    nav_links[i].addEventListener('mouseenter', toggleDropdown);
+    nav_links[i].addEventListener('mouseleave', toggleDropdown);
+}
